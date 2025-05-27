aedb9479e504a2411f0ddafd7f5b2ebc426328aa
David Cruz <david@codebycruz.com>
2025-05-26 17:16:51 -0700

    Fix uniform block abstraction

--- src/render/uniform.cpp
+++ src/render/uniform.cpp
@@ -8,10 +8,10 @@
 template <typename T> UniformBlock<T>::UniformBlock(const GLint location) : location(location) {
   glCreateBuffers(1, &bufferIdx);
   glNamedBufferStorage(bufferIdx, sizeof(T), nullptr, GL_DYNAMIC_STORAGE_BIT);
-  glBindBufferBase(GL_UNIFORM_BUFFER, location, bufferIdx);
 }
 
 template <typename T> void UniformBlock<T>::set(const T& value) const {
+  glBindBufferBase(GL_UNIFORM_BUFFER, location, bufferIdx);
   glNamedBufferSubData(bufferIdx, 0, sizeof(T), &value);
 }
 
